5382e56217a74888ef6310a8d08921a7d6d40544
longf <longf@DESKTOP-2GJ1G8J>
2022-09-14 19:45:42 +0200

    Add project files.

---
+++ Uppgift 2.12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift_2_11
+{
+    class program
+    {
+        static void Main(string[] Args)
+        {
+            int x = 300;
+            Console.WriteLine("Hur många kilometer vill du köra?");
+            string y =Console.ReadLine();
+            Console.WriteLine("Hur många dagar vill du hyra bilen?");
+            string z =Console.ReadLine();
+            int a = int.Parse(y);
+            int b = int.Parse(z);
+            Console.Write("Den totala hyran blir ");
+            Console.Write( x + a + (b * 500));
+        }
+    }
+}
